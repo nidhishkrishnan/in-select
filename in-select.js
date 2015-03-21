722ca1480b05d4761126dcc918fd925ba89d1fed
nidhishkrishnan <nidhishkrishnan@gmail.com>
2015-03-21 17:25:17 +0530

    in-select.js

---
+++ in-select.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('IndentationSelect', [])
+
+.run(function($templateCache) {
+
+				var selectHtml = "<select ng-model='model' ng-change='optionsChange()'><option ng-hide='true' ng-value='{{optionValue.value}}'>{{optionValue.value}}</option><option ng-repeat='(key, value) in selectList' value='{{value}}' style='font-weight: bold'>{{value | space}}</option></select>";
+
+				$templateCache.put("select.html", selectHtml);
+		})
+		.filter('space', function() {
+				return function(text) {
+						if (text.mainId === undefined) {
+								return text.value;
+						} else {
+								return '\u00A0\u00A0' + text.value;
+						}
+				};
+		})
+		.directive('inSelect', function() {
+				return {
+						restrict: 'E',
+						scope: {
+								ngModel: '=',
+								selectList: '='
+						},
+						templateUrl: 'select.html',
+						link: function(scope, element, attrs) {
+								scope.optionsChange = function() {
+										scope.optionStyle = {
+												'font-weight': 'normal'
+										}
+										scope.optionValue = JSON.parse(scope.model);
+										scope.model = scope.optionValue.value;
+										scope.ngModel = scope.optionValue;
+								};
+						}
+				};
+		});
